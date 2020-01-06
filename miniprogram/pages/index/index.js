29dbacfa66a55868ec24dc0080b928d22ce4e61b
wangwenxinnick <59383168+wangwenxinnick@users.noreply.github.com>
2020-01-07 10:23:42 +1030

    Update index.js

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,16 +1,26 @@
+const app = getApp()
 Page({
   //数据源
   data: {
     // movies 是放集合结果的
     movies: [],
+    counterId: '',
+    openid: '',
+    count: null,
     loading: false,
     limit: 6,
     windowHeight: 0,
-    scrollTop: 100
+    scrollTop: 100,
+    image:""
   },
 
   // 页面初始化
   onLoad: function () {
+    if (app.globalData.openid) {
+      this.setData({
+        openid: app.globalData.openid
+      })
+    }
     var _this=this;
   // 创建数据库实例
   const db = wx.cloud.database({
@@ -27,6 +37,47 @@ Page({
     }
   })
   },  
+
+
+  onRemove: function(e) {
+    wx.showModal({
+      title: '删除此记录',
+      content: '删除之后将无法恢复，确认要删除此记录？',
+      confirmText: "确定",
+      cancelText: "取消",
+      success: function (res) {
+        console.log(res);
+        if (res.confirm) {
+          const db = wx.cloud.database()
+          db.collection('userbills').doc(e.currentTarget.dataset.id).remove({
+            success: res => {
+              wx.showToast({
+                 title: '删除成功',
+               })
+               this.setData({
+                 count: null,
+                 counterId:''
+               })
+             },
+             fail: err => {
+              wx.showToast({
+                 icon: 'none',
+                 title: '删除失败',
+               })
+               console.error('[数据库] [删除记录] 失败：', err)
+             }
+           })
+        }
+      }
+    })
+       
+     
+  },
+
+
+
+
+
 
   // 页面显示（一个页面只会调用一次）
   onShow:function(){
